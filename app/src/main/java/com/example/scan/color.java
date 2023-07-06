939a3a3f3ec10e7938cf5b72ee9a057153aec8f4
vinciqt <roavicivanmateovillanueva@gmail.com>
2023-07-06 21:39:05 +0800

    shapes color sounds and images

--- app/src/main/java/com/example/scan/color.java
+++ app/src/main/java/com/example/scan/color.java
@@ -11,7 +11,7 @@ import android.widget.ImageButton;
 public class color extends AppCompatActivity {
 
     private ImageButton button;
-    private ImageButton btnblue, btnred, btnyellow;
+    private ImageButton btnblue, btnred, btnyellow, btnorange, btnviolet, btngreen;
 
     private ImageButton imgbtnvoiceout;
 
@@ -33,21 +33,42 @@ public class color extends AppCompatActivity {
         btnblue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new ShapeDialog(R.drawable.star, R.string.txt_blue, R.raw.blue).show(getSupportFragmentManager(), ShapeDialog.TAG);
+                new ShapeDialog(R.drawable.blue, R.string.txt_blue, R.raw.blue).show(getSupportFragmentManager(), ShapeDialog.TAG);
             }
         });
         btnred = findViewById(R.id.btnred);
         btnred.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new ShapeDialog(R.drawable.square, R.string.txt_red, R.raw.red).show(getSupportFragmentManager(), ShapeDialog.TAG);
+                new ShapeDialog(R.drawable.red, R.string.txt_red, R.raw.red).show(getSupportFragmentManager(), ShapeDialog.TAG);
             }
         });
         btnyellow = findViewById(R.id.btnyellow);
         btnyellow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new ShapeDialog(R.drawable.square, R.string.txt_yellow, R.raw.yellow).show(getSupportFragmentManager(), ShapeDialog.TAG);
+                new ShapeDialog(R.drawable.yellow, R.string.txt_yellow, R.raw.yellow).show(getSupportFragmentManager(), ShapeDialog.TAG);
+            }
+        });
+        btnorange = findViewById(R.id.btnorange);
+        btnorange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ShapeDialog(R.drawable.orange, R.string.txt_orange, R.raw.orange).show(getSupportFragmentManager(), ShapeDialog.TAG);
+            }
+        });
+        btnviolet = findViewById(R.id.btnviolet);
+        btnviolet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ShapeDialog(R.drawable.violet, R.string.txt_violet, R.raw.violet).show(getSupportFragmentManager(), ShapeDialog.TAG);
+            }
+        });
+        btngreen = findViewById(R.id.btngreen);
+        btngreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ShapeDialog(R.drawable.green, R.string.txt_green, R.raw.green).show(getSupportFragmentManager(), ShapeDialog.TAG);
             }
         });
     }
--- app/src/main/java/com/example/scan/shape.java
+++ app/src/main/java/com/example/scan/shape.java
@@ -13,7 +13,7 @@ import android.widget.ImageButton;
 public class shape extends AppCompatActivity {
 
     private ImageButton button;
-    private ImageButton btnstar, btnsquare;
+    private ImageButton btnstar, btnsquare, btncircle, btnrectangle, btndiamond, btntriangle, btnheart, btnoval;
 
     private ImageButton imgbtnvoiceout;
 
@@ -46,6 +46,48 @@ public class shape extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 new ShapeDialog(R.drawable.square, R.string.txt_square, R.raw.square).show(getSupportFragmentManager(), ShapeDialog.TAG);
+            }
+        });
+        btncircle = findViewById(R.id.btncircle);
+        btncircle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ShapeDialog(R.drawable.circle, R.string.txt_circle, R.raw.circle).show(getSupportFragmentManager(), ShapeDialog.TAG);
+            }
+        });
+        btnrectangle = findViewById(R.id.btnrectangle);
+        btnrectangle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ShapeDialog(R.drawable.rectangle, R.string.txt_rectangle, R.raw.rectangle).show(getSupportFragmentManager(), ShapeDialog.TAG);
+            }
+        });
+        btndiamond = findViewById(R.id.btndiamond);
+        btndiamond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ShapeDialog(R.drawable.diamond, R.string.txt_diamond, R.raw.diamond).show(getSupportFragmentManager(), ShapeDialog.TAG);
+            }
+        });
+        btntriangle = findViewById(R.id.btntriangle);
+        btntriangle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ShapeDialog(R.drawable.triangle, R.string.txt_triangle, R.raw.triangle).show(getSupportFragmentManager(), ShapeDialog.TAG);
+            }
+        });
+        btnheart = findViewById(R.id.btnheart);
+        btnheart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ShapeDialog(R.drawable.heart, R.string.txt_heart, R.raw.heart).show(getSupportFragmentManager(), ShapeDialog.TAG);
+            }
+        });
+        btnoval = findViewById(R.id.btnoval);
+        btnoval.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ShapeDialog(R.drawable.oval, R.string.txt_oval, R.raw.oval).show(getSupportFragmentManager(), ShapeDialog.TAG);
             }
         });
     }
